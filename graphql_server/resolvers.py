90f486896ab369f0f9fb7a203668a9fb16bd22e1
SRubtsov <frexrub@gmail.com>
2024-06-21 09:02:45 +0300

    edit get_tasks

--- graphql_server/resolvers.py
+++ graphql_server/resolvers.py
@@ -2,18 +2,32 @@ from strawberry import ID
 from . import schemas
 from typing import List
 
+from model import models
+from model.database import DBSession
+
 
 class QueryResolver:
-    @staticmethod
-    def get_tasks(pagination: any) -> List[schemas.Task]:
-        # TODO: update the pagination type
-        # TODO: Connect to the data layer
-        pass
 
     @staticmethod
-    def get_task(task_id: ID) -> (schemas.Task | None):
-        # TODO: Connect to the data layer
-        pass
+    def get_tasks(pagination: (schemas.PaginationInput | None) = None) -> List[schemas.Task]:
+        db = DBSession()
+        try:
+            query = db.query(models.Task)
+            if pagination is not None:
+                query = query \
+                    .offset(pagination.offset) \
+                    .limit(pagination.limit)
+
+                tasks = query.all()
+        finally:
+            db.close()
+        return tasks
+
+
+@staticmethod
+def get_task(task_id: ID) -> (schemas.Task | None):
+    # TODO: Connect to the data layer
+    pass
 
 
 class MutationResolver:
